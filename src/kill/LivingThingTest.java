932fc1daf4f004e69b52f2104d7105718d012297
epic of remnant <e185708@ie.u-ryukyu.ac.jp>
2018-11-12 01:03:06 +0900

    report3

--- src/kill/LivingThingTest.java
+++ src/kill/LivingThingTest.java
@@ -5,39 +5,69 @@ public class LivingThingTest {
     private int hitPoint;
     private int attack;
     private boolean dead;
-    private String rival;
 
-    public LivingThingTest(String name, int maximumHP, int attack) {
+    /**
+     * コンストラクタ。名前、最大HP、攻撃力を指定する。
+     * @param name ヒーロー名
+     * @param hitPoint ヒーローのHP
+     * @param attack ヒーローの攻撃力
+     */
+    public LivingThingTest(String name, int hitPoint, int attack){
         this.name = name;
-        hitPoint = maximumHP;
+        this.hitPoint = hitPoint;
         this.attack = attack;
         dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, hitPoint, attack);
     }
 
+    /**
+     * getterメソッドと同等。生死をboolean表現しているためメソッド名をisDead()とした。
+     * @return boolean
+     */
     public boolean isDead() {
         return dead;
     }
 
-    public String getName() {
+    /**
+     * 攻撃するメソッド。
+     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
+     * @param opponent 攻撃対象
+     */
+    public void attack(LivingThingTest opponent) {
+        if (opponent.dead == false && this.dead == false) {
+            int damage = (int) (Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, opponent.getName(), damage);
+            opponent.wounded(damage);
+        }
+    }
+
+    /**
+     * 自身へ攻撃されたときのダメージ処理をするメソッド。
+     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     * @param damage 受けたダメージ
+     */
+    public void wounded(int damage){
+        hitPoint -= damage;
+        if( hitPoint < 0 ) {
+            dead = true;
+            System.out.printf("%sは倒れた。\n", name);
+        }
+    }
+
+    public  int getHitPoint(){
+        return hitPoint;
+    }
+
+    public void setDead(boolean boo){
+        this.dead = boo;
+    }
+
+    public  String getName(){
         return name;
     }
 
-    public void attack(LivingThingTest rival) {
-        for (int i = 0; i == 0 && isDead() == false; i++) {
-            int damage = (int) (Math.random() * attack);
-            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, rival.getName(), damage);
-            rival.wounded(damage);
-        }
+    public void cal_damage(int damame){
+        hitPoint -= damame;
     }
 
-    public void wounded(int damage) {
-        hitPoint -= damage;
-        if (hitPoint < 0) {
-            dead = true;
-        }if(name.equals("勇者")){
-            System.out.printf("勇者%sは道半ばで力尽きてしまった。\n", name);
-        }
-        System.out.printf("モンスター%sは倒れた。\n", name);
-    }
 }
